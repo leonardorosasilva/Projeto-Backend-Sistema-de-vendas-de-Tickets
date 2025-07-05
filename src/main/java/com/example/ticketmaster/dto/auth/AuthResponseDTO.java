7bd7c3fd2ac39455ff2baf96477dd73a1011cc96
leonardorosasilva <03466316022@edu.udesc.br>
2025-07-05 12:28:00 -0300

    feat(dto) add: Para a resposta de autenticação (contém o token JWT).

---
+++ src/main/java/com/example/ticketmaster/dto/auth/AuthResponseDTO.java
@@ -0,0 +1,16 @@
+package com.example.ticketmaster.dto.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthResponseDTO {
+    private String token;
+    private String message;
+    private String tokenType = "Bearer"; // Tipo de token padrão
+}
