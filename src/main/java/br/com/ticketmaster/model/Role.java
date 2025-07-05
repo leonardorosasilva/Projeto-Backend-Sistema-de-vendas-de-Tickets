38db339aec32aa42230dfe8aeea75b77a732f05c
leonardorosasilva <03466316022@edu.udesc.br>
2025-07-05 06:50:48 -0300

    Todas as Role possiveis para o usuario

---
+++ src/main/java/br/com/ticketmaster/model/Role.java
@@ -0,0 +1,30 @@
+package br.com.ticketmaster.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@Getter
+@Setter
+@Table(name = "roles")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(unique = true, nullable = false)
+    private RoleName name;
+
+    public enum RoleName {
+        ROLE_USER, // --> USUARIO COMUM
+        ROLE_ORGANIZER, // --> ORGANIZADOR DE EVENTOS
+        ROLE_ADMIN // --> ADMINISTRADOR DO SISTEMA, ACESSO TOTAL
+    }
+}
