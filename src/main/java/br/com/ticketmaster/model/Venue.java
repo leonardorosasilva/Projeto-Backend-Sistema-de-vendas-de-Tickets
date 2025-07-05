7f0d64b6f61612a01db4d2307380a3c1b4494732
leonardorosasilva <03466316022@edu.udesc.br>
2025-07-05 06:56:35 -0300

    arquivo venue estruturado

---
+++ src/main/java/br/com/ticketmaster/model/Venue.java
@@ -0,0 +1,35 @@
+package br.com.ticketmaster.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "venues")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Venue {
+
+    @Id
+    @GeneratedValue( strategy = jakarta.persistence.GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String address;
+
+    private String city;
+
+    private String state;
+
+    private String zipcode;
+
+    @Column(nullable = false)
+    private Integer capacity; // capacidade do evento
+}
