3b5fa1d92a8b2ab6cd3d9cdf09e0f706eb5d9742
leonardorosasilva <03466316022@edu.udesc.br>
2025-07-05 12:05:43 -0300

    Exception

---
+++ src/main/java/br/com/ticketmaster/exception/BusinessException.java
@@ -0,0 +1,11 @@
+package br.com.ticketmaster.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST) // Retorna 400 Bad Request por padrão
+public class BusinessException extends RuntimeException {
+    public BusinessException(String message) {
+        super(message);
+    }
+}
