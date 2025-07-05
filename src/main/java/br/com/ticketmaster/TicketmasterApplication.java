e071adafdcb8d2b506491a1c2bd6ee01d18cfa4a
leonardorosasilva <03466316022@edu.udesc.br>
2025-07-05 06:38:44 -0300

    projeto e dependencias criadas

---
+++ src/main/java/br/com/ticketmaster/TicketmasterApplication.java
@@ -0,0 +1,15 @@
+package br.com.ticketmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.beans.BeanProperty;
+
+@SpringBootApplication
+public class TicketmasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketmasterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/ticketmaster/TicketmasterApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ticketmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketmasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
